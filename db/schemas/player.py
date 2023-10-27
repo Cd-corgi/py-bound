a610a7525cc4092ecf554893a0b4140827c9d405
cd-corgi <systemjpc@hotmail.com>
2023-10-27 10:47:34 -0500

    Editing Player Schema

--- db/schemas/player.py
+++ db/schemas/player.py
@@ -1,1 +1,31 @@
-# coming soon!
+import pymongo
+import os
+import json
+
+def Pschema():
+    PlayerSchema = {
+        "name": {'type': 'string'},
+        "genero": {'type': 'string'},
+        "gusto": {'type': 'string'},
+        "color": {'type': 'int'},
+        "combate": {'type': 'int'},
+        "inventario": [{
+            "nombre": {'type': 'string'},
+            "cantidad": {'type': 'int'}
+        }],
+        "estadistica": {
+            "xp": { 'type': 'int', 'default': 0 },
+            "limiteXP": { 'type': 'int', 'default': 50 },
+            "nivel": { 'type': 'int', 'default': 1 },
+            "ataque": { 'type': 'int', 'default': 5 },
+            "defensa": { 'type': 'int', 'default': 5 },
+            "vida": { 'type': 'int', 'default': 25 },
+        },
+        "lugarDeGuardado": {
+            "lugar": { 'type': 'string' },
+            "zona": { 'type': 'string' },
+        },
+        "listaDeEventos": [{}]
+    }
+    
+    return PlayerSchema
